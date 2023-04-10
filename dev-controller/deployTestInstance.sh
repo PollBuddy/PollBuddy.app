d54df645b97c62050cef69562caa8d8ad72e34c9
hbh7 <hbh7@hbh7.com>
2023-04-09 22:50:46 -0400

    Fixed dev controller port assignment, DB scaling in instance creation

--- dev-controller/deployTestInstance.sh
+++ dev-controller/deployTestInstance.sh
@@ -118,7 +118,7 @@ sleep 5
 # Scale the DB replicas to 1 instead of the default 3, since apparently Kubernetes has rather low pod limits per node,
 # and it's not unlikely we'll hit those limits with 3 DBs per instance. Also, they're just test instances, they don't
 # actually need 3 DBs.
-kubectl scale statefulset "poll-buddy-db-statefulset-${ID}" --replicas 1
+kubectl scale statefulset "poll-buddy-db-statefulset-${ORIGINALID}" --replicas 1
 
 # We're done!
 echo "Instance is now running"
